58e37a7c8c4ba3136cc0c091b429bf729571dcb9
Ivan <vanyok.korobka@ukr.net>
2021-11-06 00:34:06 +0200

    added new functionally in list item block

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,6 +6,14 @@ import EmployersList from '../employers-list/employers-list';
 import EmployersAddForm from '../employers-add-form/employers-add-form';
 
 function App() {
+
+    const data = [
+        {name: 'David', salary: 1000, increase: true, id: 1},
+        {name: 'Sergey', salary: 600, increase: false, id: 2},
+        {name: 'Kirill', salary: 1200, increase: false, id: 3},
+        {name: 'Ivan', salary: 5000, increase: false, id: 4}
+    ];
+
     return (
         <div className="app">
             <AppInfo/>
@@ -15,7 +23,7 @@ function App() {
                 <AppFilter/>
             </div>
 
-            <EmployersList/>
+            <EmployersList data={data}/>
             <EmployersAddForm/>
         </div>
     );
