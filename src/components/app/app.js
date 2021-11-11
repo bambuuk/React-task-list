0a8aa9e59e691db65582aa03a597ecd4e5cc82b7
Ivan <vanyok.korobka@ukr.net>
2021-11-11 23:59:15 +0200

    added filtering functionality for employees

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -12,14 +12,17 @@ class App extends Component {
         super(props);
         this.state = {
             data: [
-                {name: 'David', salary: 5000, increase: true, rise: true, id: 1},
-                {name: 'Sergey', salary: 5000, increase: false, rise: false, id: 2},
-                {name: 'Kirill', salary: 5000, increase: false, rise: false, id: 3},
-                {name: 'Ivan', salary: 5000, increase: false, rise: false, id: 4}
-            ]
+                {name: 'David Livak', salary: 800, increase: true, rise: true, id: 1},
+                {name: 'Sergey Sayenko', salary: 1001, increase: false, rise: false, id: 2},
+                {name: 'Kirill Tsonev', salary: 900, increase: false, rise: false, id: 3},
+                {name: 'Ivan Korobka', salary: 3701, increase: false, rise: false, id: 4}
+            ],
+            term: '',
+            filter: 'all'
         }
     }
 
+    // удаление ел списка сотрудников
     deleteItem = (id) => {
         this.setState(({data}) => {
             // const index = data.findIndex(elem => elem.id === id);
@@ -28,7 +31,8 @@ class App extends Component {
             }
         })
     }
-
+    
+    // добавление ел списка сотрудников
     addItem = ({name, salary}) => {
         this.setState(({data}) => {
             const dataCopy = [...data];
@@ -46,6 +50,10 @@ class App extends Component {
        })
     }
 
+    /** функция для передачи в data (состояниене этого компонента) 
+    * параметров increase и rise, которые нужны
+    * для подсчета сотрудников в компоненте AppInfo
+    */
     onToggleProp = (id, prop) => {
         this.setState(({data}) => ({
             data: data.map(item => {
@@ -57,20 +65,56 @@ class App extends Component {
         }))
     }
 
+    /** фильтрация данных в data (состояниене этого компонента) по term 
+     * (заданному поисковому запросу юзера) компонента search-panel  */
+    searchEmp = (items, term) => {
+        if (term.length === 0) {
+            return items;
+        }
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1
+        })
+    }
+
+    /** Функция добавления значения поискового запроса юзера в 
+     * data (состояниене этого компонента)*/
+    onUpdateSearch = (term) => {
+        this.setState({term: term});
+    }
+
+    /** Функция фильтрации данных пользователем */
+    filterPost = (items, filter) => {
+        switch (filter) {
+            case 'rise' :
+                return items.filter(item => item.rise);
+            case 'moreThen1000':
+                return items.filter(item => item.salary > 1000);
+            default:
+                return items;
+        }
+    }
+
+    onFilterSelect = (filter) => {
+        this.setState({filter: filter});
+    }
+
     render() {
+        const {data, term, filter} = this.state;
         const employees = this.state.data.length;
         const increased = this.state.data.filter(item => item.increase).length;
+        const visibleData = this.filterPost(this.searchEmp(data, term), filter);
+
         return (
             <div className="app">
                 <AppInfo employees={employees} increased={increased}/>
     
                 <div className="search-panel">
-                    <SearchPanel/>
-                    <AppFilter/>
+                    <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
+                    <AppFilter filter={filter} onFilterSelect={this.onFilterSelect}/>
                 </div>
     
                 <EmployersList 
-                    data={this.state.data} 
+                    data={visibleData} 
                     onDelete={this.deleteItem}
                     onToggleProp={this.onToggleProp}/>
                 <EmployersAddForm addItem={this.addItem}/>
